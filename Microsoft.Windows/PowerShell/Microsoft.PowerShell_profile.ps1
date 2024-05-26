3d43bd90a8216ad787edbddbb2f1b671321f303c
Irumyuui <ciel_katana@outlook.com>
2024-05-26 11:46:44 +0800

    add 'FZF_DEFAULT_COMMAND'

--- Microsoft.Windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Microsoft.Windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -245,4 +245,5 @@ $env:RUSTUP_UPDATE_ROOT = 'https://rsproxy.cn/rustup'
 $env:GO111MODULE = "on"
 $env:GOPROXY = "https://mirrors.aliyun.com/goproxy/,direct"
 
+$env:FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.sass-cache,node_modules,build} --type f"
 $env:FZF_DEFAULT_OPTS='--preview "bat --style=numbers --color=always --line-range :500 {}"'
